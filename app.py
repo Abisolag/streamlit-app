69fb7aa2a7584015ef46cc23fc6c04d7d2d8b5a9
VICTORIA ABISOLA <138674570+Abisolag@users.noreply.github.com>
2024-09-20 04:25:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
-!import pandas as pd
-!import matplotlib.pyplot as plt
-!import seaborn as sns
-!import plotly.express as px
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
 
 df=pd.read_csv('new_data.csv')
 
